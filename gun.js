6eec8c94156635bc868e525c222c2e0677c08661
7iambatman7 <120754314+7iambatman7@users.noreply.github.com>
2025-02-13 14:35:53 -0600

    Update gun.js

--- gun.js
+++ gun.js
@@ -94,7 +94,7 @@
                     type: "block",
                     id: "iron_block" // Using dirt blocks
                 },
-                "Q": {
+                "G": {
                     type: "item",
                     id: "gunpowder" // Using dirt blocks
                 },
@@ -102,8 +102,9 @@
     
             // Define the crafting grid pattern for the recipe
             var recipePattern = [
-                "IIC",
-                " QI"
+                "III",
+                "III"
+                "GII"
             ];
     
             // Convert the recipe pattern and legend into the required format
