b484ae9a3d1f7905818bfe6fef062f82e58cc5f2
7iambatman7 <120754314+7iambatman7@users.noreply.github.com>
2025-02-13 11:57:31 -0600

    Update gun.js

--- gun.js
+++ gun.js
@@ -14,7 +14,7 @@
         var itemSuper = ModAPI.reflect.getSuper(itemClass, (x) => x.length === 1);
         var nmi_ItemPistol = function nmi_ItemPistol() {
             itemSuper(this); //Use super function to get block properties on this class.
-            this.$setCreativeTab(creativeMiscTab);
+            this.$setCreativeTab(creativeToolTab);
         }
 
         function entityRayCast(player, world, range) {
